93c854c8c3f62b537473aca21beafaad6bb819ab
tboesche <74301725+tboesche@users.noreply.github.com>
2024-07-28 08:51:10 +0200

    Add files via upload

---
+++ src/ai.rs
@@ -0,0 +1,3 @@
+pub mod random_player;
+pub mod classic_player;
+pub mod simple_nn;
---
+++ src/frontend.rs
@@ -0,0 +1,14 @@
+pub mod draw_board;
+pub mod aux_functions;
+pub mod coords;
+
+pub mod io;
+pub mod board_parameters;
+pub mod tiles;
+pub mod circles;
+pub mod nodes;
+pub mod edges;
+pub mod buildings;
+pub mod harbors;
+pub mod roads;
+pub mod actions;
